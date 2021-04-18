096065505b6092a32b755fd32d691f1a895321ff
Muhammed Abdrahman <muhammed03amin@gmail.com>
2021-04-18 15:42:57 +0600

    feat: add duration time status, add volume mode button, add playlist list

--- script.js
+++ script.js
@@ -4,9 +4,15 @@ const playBtn = document.getElementById("play");
 const prevBtn = document.getElementById("prev");
 const nextBtn = document.getElementById("next");
 const modeBtn = document.getElementById("mode");
+const volumeBtn = document.getElementById("volume");
 
 const modeIcon = document.getElementById("mode-icon");
+const volumeIcon = document.getElementById("volume-icon");
 
+const timestamp = document.getElementById("current-time");
+const durationTime = document.getElementById("duration-time");
+
+const playlist = document.getElementById("playlist");
 const audio = document.getElementById("audio");
 const progress = document.getElementById("progress");
 const progressContainer = document.getElementById("progress-container");
@@ -16,11 +22,11 @@ const cover = document.getElementById("cover");
 
 // Audio titles
 const audios = [
-  { songName: "If you ask me", author: "Yusuf Islam" },
-  { songName: `Al-Mu'allim`, author: "Sami Yusuf" },
-  { songName: "Allahu Allah", author: "Sami Yusuf" },
-  { songName: "Forgotten Promises", author: "Yusuf Islam" },
-  { songName: "Give Thanks", author: "Michael Jackson" },
+  { songName: "If you ask me", author: "Yusuf Islam", id: 1 },
+  { songName: `Al-Mu'allim`, author: "Sami Yusuf", id: 2 },
+  { songName: "Allahu Allah", author: "Sami Yusuf", id: 3 },
+  { songName: "Forgotten Promises", author: "Yusuf Islam", id: 4 },
+  { songName: "Give Thanks", author: "Michael Jackson", id: 5 },
 ];
 
 // Modes
@@ -33,6 +39,55 @@ let audioIndex = 0;
 let modeIndex = 0;
 let modeStatus = modes[modeIndex];
 
+//load playlist items
+
+function loadPlaylistItems() {
+  audios.map((item) => {
+    let playlistItem = document.createElement("div");
+    let audioName = document.createElement("span");
+    let songAuthor = document.createElement("span");
+
+    playlist.appendChild(playlistItem);
+
+    audioName.innerText = `${item.songName}`;
+    songAuthor.innerText = `${item.author}`;
+    playlistItem.appendChild(audioName);
+    playlistItem.appendChild(songAuthor);
+    playlistItem.id = item.id;
+  });
+}
+
+loadPlaylistItems();
+//
+document.addEventListener(
+  "click",
+  function (e) {
+    e = e || window.event;
+    let target = e.srcElement;
+
+    if (
+      target.parentNode.id === "playlist" ||
+      target.parentNode.parentNode.id === "playlist"
+    ) {
+      if (target.parentNode.id === "playlist") {
+        console.log(target.id);
+        audioIndex = target.id - 1;
+
+        loadAudio(audios[audioIndex]);
+
+        playSong();
+      } else {
+        console.log(target.parentNode.id);
+        audioIndex = target.parentNode.id - 1;
+
+        loadAudio(audios[audioIndex]);
+
+        playSong();
+      }
+    }
+  },
+  false
+);
 // Initially load audiu details into DOM
 loadAudio(audios[audioIndex]);
 
@@ -146,11 +201,56 @@ function nextSongPlayMode() {
 
   return playMode;
 }
+
+// Set volume
+let volumeMode = true;
+function setVolume() {
+  if (volumeMode) {
+    audio.volume = 0;
+    volumeMode = false;
+    volumeIcon.classList.remove("fa-volume-up");
+    volumeIcon.classList.add("fa-volume-mute");
+  } else {
+    audio.volume = 1;
+    volumeMode = true;
+    volumeIcon.classList.add("fa-volume-up");
+    volumeIcon.classList.remove("fa-volume-mute");
+  }
+}
 // Update progress bar
 function updateProgress(e) {
   const { duration, currentTime } = e.srcElement;
   const progressPercent = (currentTime / duration) * 100;
   progress.style.width = `${progressPercent}%`;
+
+  // Get minutes
+  let mins = Math.floor(audio.currentTime / 60);
+  if (mins < 10) {
+    mins = "0" + String(mins);
+  }
+
+  // Get seconds
+  let secs = Math.floor(audio.currentTime % 60);
+  if (secs < 10) {
+    secs = "0" + String(secs);
+  }
+
+  let durationMins = Math.floor(duration / 60);
+  if (durationMins < 10) {
+    durationMins = "0" + String(durationMins);
+  }
+
+  let durationSecs = Math.floor(duration / 60);
+  if (durationSecs < 10) {
+    durationSecs = "0" + String(durationSecs);
+  }
+
+  timestamp.innerHTML = `${mins}:${secs}`;
+
+  setTimeout(
+    (durationTime.innerHTML = `${durationMins}:${durationSecs}`),
+    10000
+  );
 }
 
 function setProgress(e) {
@@ -181,4 +281,8 @@ modeBtn.addEventListener("click", checkMode);
 audio.addEventListener("timeupdate", updateProgress);
 progressContainer.addEventListener("click", setProgress);
 
+// End audio
 audio.addEventListener("ended", nextSongPlayMode);
+
+// Volume
+volumeBtn.addEventListener("click", setVolume);
